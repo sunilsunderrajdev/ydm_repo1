bd1866dd3aff0ebb2a5375cbe6c66b4a33113fb4
Sunil Sunder Raj <sunilsunderrajdev@gmail.com>
2023-12-04 15:05:31 -0500

    Hardcoded subnet ID

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2ydm" {
     ami = "ami-0230bd60aa48260c6"
     instance_type = "t2.micro"
 
-    subnet_id = element(aws_subnet.private_subnets.id, 0)
+    subnet_id = "subnet-0d708126c0a1a01f3"
     user_data = <<EOF
 #!/bin/bash
 echo "Copying the SSH Key to the server"
