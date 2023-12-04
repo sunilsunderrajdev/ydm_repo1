d4b3986c71c95d30b98fe365b79e07a1a803f79c
Sunil Sunder Raj <sunilsunderrajdev@gmail.com>
2023-12-03 19:43:50 -0500

    Networking changes

---
+++ nat.tf
@@ -0,0 +1,53 @@
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id     = "${element(aws_subnet.public_subnet.*.id, 0)}"
+  depends_on    = [aws_internet_gateway.ig]
+  tags = {
+    Name        = "Public nat"
+  }
+}
+
+/* Routing table for private subnet */
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Name        = "private-route-table"
+  }
+}
+/* Routing table for public subnet */
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Name        = "public-route-table"
+  }
+}
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = "${aws_route_table.private.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+}
+
+/* Route table associations */
+resource "aws_route_table_association" "public" {
+  count          = "${length(["10.0.1.0/24","10.0.2.0/24"])}"
+  subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private" {
+  count          = "${length(["10.0.3.0/24","10.0.4.0/24"])}"
+  subnet_id      = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
