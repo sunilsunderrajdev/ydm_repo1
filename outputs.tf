16142de58aa3b616aba78335e3f99eb891caea51
Sunil Sunder Raj <sunilsunderrajdev@gmail.com>
2023-12-05 20:36:49 -0500

    Set prinate_key as sensitive

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
+output "public_key" {
+  value       = join("", tls_private_key.EC2_private_key.*.public_key_openssh)
+  description = "Content of the generated public key"
+}
+
 output "private_key" {
   value       = tls_private_key.EC2_private_key.private_key_pem
   description = "Content of the generated private key"
-  sensitive   = false
+  sensitive   = true
 }
