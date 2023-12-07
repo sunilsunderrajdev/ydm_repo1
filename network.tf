e477a13368de190219817080d4637b2a5155aead
Sunil Sunder Raj <sunilsunderrajdev@gmail.com>
2023-12-07 18:08:11 -0500

    Removed hardcodings and added newlines

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.vpc_cidr
     enable_dns_hostnames = true
     enable_dns_support   = true
 
@@ -31,9 +31,9 @@ resource "aws_nat_gateway" "nat" {
 }
 
 resource "aws_subnet" "public_subnets" {
- count      = length(["10.0.1.0/24","10.0.2.0/24"])
+ count      = length(var.vpc_cidrs_public)
  vpc_id     = aws_vpc.vpc.id
- cidr_block = element(["10.0.1.0/24","10.0.2.0/24"], count.index)
+ cidr_block = element(var.vpc_cidrs_public, count.index)
  availability_zone_id = element(["use1-az1"], count.index)
  map_public_ip_on_launch = true
  
@@ -43,9 +43,9 @@ resource "aws_subnet" "public_subnets" {
 }
 
 resource "aws_subnet" "private_subnets" {
- count      = length(["10.0.3.0/24","10.0.4.0/24"])
+ count      = length(var.vpc_cidrs_private)
  vpc_id     = aws_vpc.vpc.id
- cidr_block = element(["10.0.3.0/24","10.0.4.0/24"], count.index)
+ cidr_block = element(var.vpc_cidrs_private, count.index)
  availability_zone_id = element(["use1-az1"], count.index)
  map_public_ip_on_launch = false
  
@@ -83,13 +83,13 @@ resource "aws_route" "private_nat_gateway" {
 
 /* Route table associations */
 resource "aws_route_table_association" "public" {
-  count          = "${length(["10.0.1.0/24","10.0.2.0/24"])}"
+  count          = "${length(var.vpc_cidrs_public)}"
   subnet_id      = "${element(aws_subnet.public_subnets.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
 resource "aws_route_table_association" "private" {
-  count          = "${length(["10.0.3.0/24","10.0.4.0/24"])}"
+  count          = "${length(var.vpc_cidrs_private)}"
   subnet_id      = "${element(aws_subnet.private_subnets.*.id, count.index)}"
   route_table_id = "${aws_route_table.private.id}"
 }
