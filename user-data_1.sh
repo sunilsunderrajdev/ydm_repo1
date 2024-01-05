b1cd3e24d820f5fb8d25d78f44429b4f7bc75da2
Sunil Sunder Raj <sunilsunderrajdev@gmail.com>
2024-01-05 16:48:38 -0500

    Dummy commit

--- user-data_1.sh
+++ user-data_1.sh
@@ -8,4 +8,4 @@ git clone https://github.com/gabrielecirulli/2048.git
 cp -R 2048/* /var/www/html/
 systemctl start httpd && systemctl enable httpd
 
-echo "Hello from SSR !!!!"
+echo "Hello from SSR !!!!!"
