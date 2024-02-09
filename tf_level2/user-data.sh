6d2d4636a2da0561cbfc44f4d1835158b575c200
Sunil Sunder Raj <sunilsunderrajdev@gmail.com>
2024-02-08 19:57:15 -0500

    iam roles

--- tf_level2/user-data.sh
+++ tf_level2/user-data.sh
@@ -8,4 +8,9 @@ git clone https://github.com/gabrielecirulli/2048.git
 cp -R 2048/* /var/www/html/
 systemctl start httpd && systemctl enable httpd
 
+#yum remove awscli
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#./aws/install
+
 echo "Hello from SSR !!!!!"
